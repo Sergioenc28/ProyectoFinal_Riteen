2b30ea0eefc695561bd92196c0bee509f73d21e0
Dioni Ripoll <20112496@itla.edu.do>
2012-11-15 18:33:19 -0400

    Commit de Ripoll

--- ProyectoFinal_Riteen/src/Riteen/Main.java
+++ ProyectoFinal_Riteen/src/Riteen/Main.java
@@ -14,7 +14,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Falta Ripoll
+        
+        //Llego Ripoll
         
     }
 }
